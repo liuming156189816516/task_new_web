1e1123ad568b732c2b7f5438483159db34f28a1d
2644345145@qq.com <065998lcr>
2025-09-29 19:00:33 +0800

    【前端】代码提交  枕着校验折扣字段

--- admin/src/views/releaseTask/platform/components/detailsAction.vue
+++ admin/src/views/releaseTask/platform/components/detailsAction.vue
@@ -1,14 +1,20 @@
 <!-- 发布平台-套餐 添加 编辑 -->
 <template>
   <el-dialog
-    :title="actionModal.type==='add'?'新建':'编辑'"
-    center
-    :visible.sync="actionModal.show"
     :close-on-click-modal="false"
+    :title="actionModal.type==='add'?'新建':'编辑'"
+    :visible.sync="actionModal.show"
+    center
     width="500px"
     @close="closeModal"
   >
-    <el-form ref="refActionModal" size="small" :model="actionModal.formData" label-width="100px" :rules="actionModal.rules">
+    <el-form
+      ref="refActionModal"
+      :model="actionModal.formData"
+      :rules="actionModal.rules"
+      label-width="100px"
+      size="small"
+    >
       <el-form-item label="执行类型:" prop="execution_type">
         <el-select v-model="actionModal.formData.execution_type" clearable filterable placeholder="请选择平台">
           <el-option v-for="item in executionTypeList" :key="item.value" :label="item.label" :value="item.value" />
@@ -16,16 +22,16 @@
       </el-form-item>
 
       <el-form-item label="折扣:" prop="discount">
-        <el-input v-model="actionModal.formData.discount" type="number" placeholder="请输入折扣" @input="changeInput" />
+        <el-input v-model="actionModal.formData.discount" placeholder="请输入折扣" type="number" @input="changeInput" />
       </el-form-item>
       <el-form-item label="数量:" prop="num">
-        <el-input v-model="actionModal.formData.num" type="number" placeholder="请输入数量" @input="changeInput" />
+        <el-input v-model="actionModal.formData.num" placeholder="请输入数量" type="number" @input="changeInput" />
       </el-form-item>
       <el-form-item label="扣除:" prop="deduct">
-        <el-input v-model="actionModal.formData.deduct" type="number" placeholder="请输入扣除" @input="changeInput" />
+        <el-input v-model="actionModal.formData.deduct" placeholder="请输入扣除" type="number" @input="changeInput" />
       </el-form-item>
 
-      <el-form-item label-width="0" style="text-align:center;" class="el-item-bottom">
+      <el-form-item class="el-item-bottom" label-width="0" style="text-align:center;">
         <el-button @click="closeModal">取消</el-button>
         <el-button :loading="actionModal.isLoading" type="primary" @click="submitFormFun">确认</el-button>
       </el-form-item>
@@ -39,8 +45,7 @@ import { addDetailsDataApi, editDetailsDataApi } from '../api';
 
 export default {
   name: 'DetailsAction',
-  components: {
-  },
+  components: {},
   data() {
     return {
       actionModal: {
@@ -56,7 +61,19 @@ export default {
           execution_type: [{ required: true, message: '请选择执行类型！', trigger: 'change' }],
           discount: [{ required: true, message: '请输入折扣！', trigger: 'change' }],
           num: [{ required: true, message: '请输入数量！', trigger: 'change' }],
-          deduct: [{ required: true, message: '请输入扣除！', trigger: 'change' }],
+          deduct: [
+            { required: true, message: '请输入扣除！', trigger: 'change' },
+            {
+              required: true, validator: (rule, value, callback) => {
+                const reg = /^(?:0|[1-9]\d*)$/
+                if (reg.test(value)) {
+                  callback()
+                } else {
+                  return callback(new Error('请输入正整数！'))
+                }
+              }, trigger: 'change'
+            },
+          ],
         },
         isLoading: false,
       },
@@ -68,7 +85,7 @@ export default {
     }
   },
   methods: {
-    open(row,type,parentForm) {
+    open(row, type, parentForm) {
       this.actionModal.show = true
       this.actionModal.type = type
       this.cloneRow = deepClone(parentForm)
@@ -96,7 +113,7 @@ export default {
               if (res.msg === 'success') {
                 successTips(this, 'success', '保存成功！')
                 this.$emit('updateDetailDataFun')
-              }else {
+              } else {
                 this.actionModal.isLoading = false
               }
             })
@@ -105,7 +122,7 @@ export default {
               if (res.msg === 'success') {
                 successTips(this, 'success', '編輯成功！')
                 this.$emit('updateDetailDataFun')
-              }else {
+              } else {
                 this.actionModal.isLoading = false
               }
             })
